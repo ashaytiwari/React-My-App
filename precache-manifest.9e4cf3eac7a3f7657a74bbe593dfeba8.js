df058a81be540eb0f5702f661c5910d149b31219
ashaytiwari <ashaytiwari2001@gmail.com>
2020-07-15 15:27:32 +0530

    Updates

---
+++ precache-manifest.9e4cf3eac7a3f7657a74bbe593dfeba8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11797a01931d7bc6907d0cfc752f7148",
+    "url": "/React-My-App/index.html"
+  },
+  {
+    "revision": "ec7ca527a22a77981d6f",
+    "url": "/React-My-App/static/css/main.e61cb387.chunk.css"
+  },
+  {
+    "revision": "478cffdfa64f15476aab",
+    "url": "/React-My-App/static/js/2.ca093aca.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-My-App/static/js/2.ca093aca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7ca527a22a77981d6f",
+    "url": "/React-My-App/static/js/main.4c3e8305.chunk.js"
+  },
+  {
+    "revision": "8832eb5e36c3e0e3ad56",
+    "url": "/React-My-App/static/js/runtime-main.96bd96ef.js"
+  },
+  {
+    "revision": "ab37609ba228612081dd826a7cef081d",
+    "url": "/React-My-App/static/media/Higher-Order.ab37609b.png"
+  },
+  {
+    "revision": "4658f38e54a60e4593028343fe5aec29",
+    "url": "/React-My-App/static/media/about.4658f38e.jpg"
+  }
+]);
